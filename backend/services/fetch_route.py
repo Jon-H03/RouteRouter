99f90e5c0df311f1828e55e6a235f5952eb6770d
Jonathan Hirsch <jonathan.hirsch2011@gmail.com>
2025-01-03 17:17:05 -0800

    Add function to fetch data

---
+++ backend/services/fetch_route.py
@@ -0,0 +1,24 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+RECREATION_API_KEY = os.getenv("RECREATION_API_KEY")
+
+def fetch_recreation_data(lat, lon, activity):
+    url = f"https://ridb.recreation.gov/api/v1/facilities"
+    headers = {"apikey": os.getenv("RECREATION_API_KEY")}
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "activity": activity,
+        "radius": 150,
+        "limit": 50
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": f"Failed to fetch data: {response.status_code}"}
