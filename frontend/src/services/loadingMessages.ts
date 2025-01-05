c6b99a213e8ea487960191491caf01ddd124e39d
Jonathan Hirsch <jonathan.hirsch2011@gmail.com>
2025-01-05 10:01:09 -0800

    Fix dependency array

--- frontend/src/services/loadingMessages.ts
+++ frontend/src/services/loadingMessages.ts
@@ -36,7 +36,7 @@ export const useLoadingMessages = () => {
     }, intervalMs);
 
     return () => clearInterval(messageInterval);
-  }, []);
+  }, [messages, intervalMs]);
 
   return currentMessage;
 };
